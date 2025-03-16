f9ea2b958429f4784b124d100c0d7b77504ffab9
Harshad Vilas Magadum <hvmagadum04@gmail.com>
2025-03-16 18:08:08 +0530

    Added server's url in app.js file.

--- real-time-chat/src/App.js
+++ real-time-chat/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:5000");
+const socket = io.connect("https://real-time-chat-jdmy.onrender.com/");
 
 function App() {
   const [message, setMessage] = useState("");
